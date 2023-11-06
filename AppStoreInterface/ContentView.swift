06c487a0b3b20cd07a88dbb98ebc767c9c3222e1
Ansheng Zhou <azhou@lcs.on.ca>
2023-11-06 14:41:10 -0500

    set up background

--- AppStoreInterface/ContentView.swift
+++ AppStoreInterface/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        //            first layer background color/theme
+        ZStack {
+            Color.yellow
+                .ignoresSafeArea()
+            
+            
+        }.preferredColorScheme(.dark)
+        
     }
 }
 
