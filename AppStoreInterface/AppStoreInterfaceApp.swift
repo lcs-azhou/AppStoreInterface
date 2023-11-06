c48a26dde057304879b996d5d16dfafa72e9f43f
Ansheng Zhou <azhou@lcs.on.ca>
2023-11-06 14:31:28 -0500

    Initial Commit

---
+++ AppStoreInterface/AppStoreInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreInterfaceApp.swift
+//  AppStoreInterface
+//
+//  Created by Ansheng Zhou on 2023-11-06.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
